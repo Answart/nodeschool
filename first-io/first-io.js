8616009e44a5e0b799104df0433012054eb02997
Alexandra Swart <answart@sbcglobal.net>
2014-07-01 17:39:46 -0700

    find sync readfile method

--- first-io/first-io.js
+++ first-io/first-io.js
@@ -1,4 +1,15 @@
+// 1. Require 'fs'
 //Now you have the full fs module available in a variable named fs.
 var fs = require('fs')
 
-//All synchronous (or blocking) filesystem methods in the fs module end with 'Sync'. To read a file, you'll need to use fs.readFileSync('/path/to/file'). This method will return a Buffer object containing the complete contents of the file.
+// 2a. Create single synchronous function to read file
+        //  fs.readFileSync(filename, [options])
+        // Synchronous version of fs.readFile. Returns the contents of the filename.
+
+readit = fs.readFileSync(process.argv[2]);
+//process.argv[2] = 'first-io.js' from 'learnyounode verify first-io.js'
+
+console.log(readit)
+// reads file. (returns buffer object)
+// Buffer Object: Node's way of representing arrays of data
+// "<Buffer cf fa ed fe 07 00 00 01 03 00 00 80 02 00 00 00 14 00 00 00 e8 0a 00 00 85 80 01 00 00 00 00 00 19 00 00 00 48 00 00 00 5f 5f 50 41 47 45 5a 45 52 4f 00 ...>"
